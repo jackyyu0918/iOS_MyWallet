2d28d864ba93bfaca883ac49b046efc267f13c18
andy <hhchow7-c@my.cityu.edu.hk>
2019-10-31 18:46:27 +0800

    andy temp

--- cs4298_asg2/Controller/Analysis_VC.swift
+++ cs4298_asg2/Controller/Analysis_VC.swift
@@ -9,14 +9,35 @@
 import Foundation
 import UIKit
 
-class Analysis: UIViewController {
+class Analysis_VC: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var Stat_TableView: UITableView!
+    
 
-    //ddgdgdgbsfbss
-    //drgsgsg
+    let animals:[String] = ["Bat","Bear"]
+  
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
+        }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Health", for: indexPath)
+        cell.textLabel?.text = animals[indexPath.row]
+        return cell
+    }
+    
+    
+  
 
     
 }
