88ae7b0e5112da25f1fcd23375e903f4130beb0d
andy <hhchow7-c@my.cityu.edu.hk>
2019-10-31 17:21:41 +0800

    Andy:add Analyisis_VC

---
+++ cs4298_asg2/Controller/Analysis_VC.swift
@@ -0,0 +1,21 @@
+//
+//  Analysis_VC.swift
+//  cs4298_asg2
+//
+//  Created by CHOW Ho Hin on 31/10/2019.
+//  Copyright © 2019 YU Ka Kit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Analysis: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
