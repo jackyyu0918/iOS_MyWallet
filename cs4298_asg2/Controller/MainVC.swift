001a41dbac21ffea7d7de0227b487461e6b4e2c1
Jacky YU <jackyyu.github@gmail.com>
2019-11-01 13:41:03 +0800

    Complete Side Menu

--- cs4298_asg2/Controller/MainVC.swift
+++ cs4298_asg2/Controller/MainVC.swift
@@ -12,10 +12,25 @@ class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NotificationCenter.default.addObserver(self, selector: #selector(showAnalyze), name: NSNotification.Name("ShowAnalyze"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(showCategory), name: NSNotification.Name("ShowCategory"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(showSetting), name: NSNotification.Name("ShowSetting"), object: nil)
         // Do any additional setup after loading the view.
     }
+    
+    @objc func showAnalyze() {
+        performSegue(withIdentifier: "ShowAnalyze", sender: nil)
+    }
+    
+    @objc func showCategory(){
+        performSegue(withIdentifier: "ShowCategory", sender: nil)
+    }
+    
+    @objc func showSetting(){
+        performSegue(withIdentifier: "ShowSetting", sender: nil)
+    }
 
+    
     @IBAction func onMoreTapped(){
         print("Toggle side menu")
         NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
