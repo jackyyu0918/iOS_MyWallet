7c2102e1edad103ef238139c47bbfaafc8e2276a
Andy Chow <hhchow7-c@my.cityu.edu.hk>
2019-11-02 17:15:33 +0800

    Andy: Create pie Chart

--- cs4298_asg2/Controller/Analysis_VC.swift
+++ cs4298_asg2/Controller/Analysis_VC.swift
@@ -9,32 +9,25 @@
 import Foundation
 import UIKit
 
-class Analysis_VC: UIViewController,UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet weak var Stat_TableView: UITableView!
+class Analysis_VC: UIViewController {
     
 
-    let animals:[String] = ["Bat","Bear"]
-  
     
     override func viewDidLoad() {
+        let pieChartView = PieChartView()
+        pieChartView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 400)
+        pieChartView.segments = [
+            Segment(color: .red, value: 57),
+            Segment(color: .blue, value: 30),
+            Segment(color: .green, value: 25),
+            Segment(color: .yellow, value: 40)
+        ]
+        view.addSubview(pieChartView)
         
         super.viewDidLoad()
         }
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return animals.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Health", for: indexPath)
-        cell.textLabel?.text = animals[indexPath.row]
-        return cell
-    }
+  
     
     
   
