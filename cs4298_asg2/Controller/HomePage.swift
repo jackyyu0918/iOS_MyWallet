9dd6551a5b221309c6654e71800e097c14eef463
Virgil <chanyatchau@gmail.com>
2019-11-03 15:16:50 +0800

    Update - Add button

---
+++ cs4298_asg2/Controller/HomePage.swift
@@ -0,0 +1,14 @@
+//
+//  HomePage.swift
+//  cs4298_asg2
+//
+//  Created by CHAN Yat Chau on 3/11/2019.
+//  Copyright © 2019 YU Ka Kit. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class HomePage: MainVC {
+    
+}
