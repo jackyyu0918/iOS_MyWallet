8894d0d6368b86bc3df8f80850fbe7458207f91a
Jacky YU <jackyyu.github@gmail.com>
2019-11-05 13:18:24 +0800

    nothing

--- cs4298_asg2/View/Record+CoreDataClass.swift
+++ cs4298_asg2/View/Record+CoreDataClass.swift
@@ -102,7 +102,7 @@ public class Record: NSManagedObject {
                 outcomeSum = outcomeSum + record.value;
             }else{
                 print(record.nature)
-                fatalError()
+//                fatalError()
             }
         }
         
